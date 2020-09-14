a54979927e45de26beba3a67017ca86b57f2cfac
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-09-14 13:22:03 -0400

    Add a labeling server generation script

---
+++ scripts/1-create-labeling-server.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd ..
+
+
+## Handle Input 
+print_usage() {
+  printf "use -s to specify the source dataset folder (with ending `/`). \n"
+  printf "use -t to specify the target saving folder (without starting `labeling/`). \n"
+  printf "use -c to specify the configuration file name (without starting `configs/`). \n"
+}
+
+config=''
+source_folder=''
+target_folder=''
+while getopts 'c:s:t:h' flag; do
+  case "${flag}" in
+    c) config="configs/${OPTARG}" ;;
+    s) source_folder="${OPTARG}" ;;
+    t) target_folder="labeling/${OPTARG}" ;;
+    h) print_usage
+       exit 1 ;;
+  esac
+done
+
+mkdir -p labeling
+
+# Create the labeling server
+label-studio init $target_folder \
+                --input-path=$source_folder \
+                --input-format=image-dir \
+                --allow-serving-local-files --force \
+                --label-config=configs/$config \
+                --port 8899
+
+cd ./tools 
+python generate_password.py --path ../$target_folder
