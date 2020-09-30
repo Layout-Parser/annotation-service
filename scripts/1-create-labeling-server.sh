69328ff3a91370e485d2e1cb86492c917d8b7f43
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-09-30 15:53:10 -0400

    Support specifying port numebr in labeling server creation

--- scripts/1-create-labeling-server.sh
+++ scripts/1-create-labeling-server.sh
@@ -7,16 +7,19 @@ print_usage() {
   printf "use -s to specify the source dataset folder (with ending `/`). \n"
   printf "use -t to specify the target saving folder (without starting `labeling/`). \n"
   printf "use -c to specify the configuration file name (without starting `configs/`). \n"
+  printf "use -p to specify the port number (e.g., 8899). \n"
 }
 
 config=''
 source_folder=''
 target_folder=''
-while getopts 'c:s:t:h' flag; do
+port=''
+while getopts 'c:s:t:p:h' flag; do
   case "${flag}" in
     c) config="configs/${OPTARG}" ;;
     s) source_folder="${OPTARG}" ;;
     t) target_folder="labeling/${OPTARG}" ;;
+    p) port="${OPTARG}" ;;
     h) print_usage
        exit 1 ;;
   esac
@@ -30,7 +33,8 @@ label-studio init $target_folder \
                 --input-format=image-dir \
                 --allow-serving-local-files --force \
                 --label-config=$config \
-                --port 8899
+                --port $port \
+                --ml-backends http://localhost:9090
 
 cd ./tools 
 python generate_password.py --path ../$target_folder
