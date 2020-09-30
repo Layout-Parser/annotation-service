81f46982c97594c6e92f206fa2a9642421a14636
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-09-30 15:52:46 -0400

    Add modeling server scripts

---
+++ scripts/3-create-modeling-server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd ..
+
+## Handle Input 
+print_usage() {
+  printf "use -s to specify the modeling script path. \n"
+  printf "use -t to specify the modeling folder (without starting `labeling/`). \n"
+}
+
+
+script=''
+folder=''
+while getopts 's:t:h' flag; do
+  case "${flag}" in
+    s) script="tools/${OPTARG}" ;;
+    t) folder="labeling/${OPTARG}" ;;
+    h) print_usage
+       exit 1 ;;
+  esac
+done
+
+
+label-studio-ml $folder --script $script 
